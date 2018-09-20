618b9621d25af5b188a1d9f6e2f3500f946d4111
YingQQQ <yingqi2008win@gmail.com>
2018-09-20 17:37:57 +0800

    complete chapter-1-1.32

--- chapter_1_part4.rkt
+++ chapter_1_part4.rkt
@@ -36,7 +36,7 @@
 (sum-fast-cubes 1 4)
 
 
-;execrise 1.29 汤普森规则计算
+;exercise 1.29 汤普森规则计算
 
 (define (odd? n)
   (= (remainder n 2) 0))
@@ -86,6 +86,144 @@
   (fast-sum cube a inc b))
 
 (sum-faster-cubes 1 100)
+
+;; exercise 1.31
+
+;递归版本
+(define (product term a next b)
+  (if (> a b)
+      1 ;因为是乘积所以最后一个值不能是0
+      (* (term a)
+         (product term (next a) next b))))
+  
+;;lambda匿名函数声明
+(product (lambda (x) x)
+         1
+         (lambda (y) (+ y 1))
+         10)
+;迭代版本
+(define (fast-product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result ;因为是乘积所以最后一个值不能是0
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(fast-product (lambda (x) x)
+         1
+         (lambda (y) (+ y 1))
+         10)
+;;重新定义乘阶函数
+(define (factorial n)
+  (fast-product (lambda (x) x)
+         1
+         (lambda (y) (+ y 1))
+         n))
+
+(define (bottom-iter i)
+  (cond ((= i 1)
+         3)
+        ((odd? i)
+         (+ i 1))
+        (else (+ i 2))))
+
+(define (top-iter n)
+  (cond ((= n 1)
+         2)
+        ((odd? n)
+         (+ n 2))
+        (else (+ n 1))))
+
+(define (pi n)
+  (* 4
+     (exact->inexact (/ (fast-product top-iter
+                                      1
+                                      (lambda (i) (+ i 1))
+                                      n)
+                        (fast-product bottom-iter
+                                      1
+                                      (lambda (i) (+ i 1))
+                                      n)))))
+  
+(pi 1000)
+
+;;exercise 1.32
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner
+                            null-value
+                            term
+                            (next a)
+                            next
+                            b))))
+
+(define (resum term a next b)
+  (accumulate +
+              0
+              term
+              a
+              next
+              b))
+(define (reproduct term a next b)
+  (accumulate *
+              1
+              term
+              a
+              next
+              b))
+
+;递归版本
+(define (fast-accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a))))) 
+  (iter a null-value))
+
+
+;; 素数检查 费马小定律
+(define (square x)
+  (* x x))
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((odd? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+         
+(define (prime-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0)
+         true)
+        ((prime-test n)
+         (fast-prime? n (- times 1)))
+        (else false)))
+(define (prime? n)
+  (fast-prime? n 10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
