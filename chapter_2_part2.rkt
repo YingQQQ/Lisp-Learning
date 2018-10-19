1e3a758cbb8895e987fef09c1fc72391dfbf75da
YingQQQ <yingqi2008win@gmail.com>
2018-10-19 16:57:09 +0800

    exercise-2.9-done

--- chapter_2_part2.rkt
+++ chapter_2_part2.rkt
@@ -169,6 +169,72 @@
 |#
 
 
+;2.14 扩展练习:区间算术
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+;2.7
+(define (make-interval a b)
+  (cons a b))
+
+(define (lower-bound r)
+  (car r))
+
+(define (upper-bound r)
+  (cdr r))
+
+;exercise-2.8
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+;test
+(define (display-info r)
+  (newline)
+  (display "[")
+  (display (lower-bound r))
+  (display ",")
+  (display (upper-bound r))
+  (display "]"))
+(define a (make-interval 1 10))
+(define b (make-interval 50 100))
+(define test1 (sub-interval b a))
+(display-info test1)
+
+;exercise-2.9
+
+(define add-interval-test (add-interval b a))
+(display-info add-interval-test)
+
+(define (width-interval interval)
+  (/ (- (upper-bound interval)
+        (lower-bound interval))
+     2.0))
+(newline)
+(width-interval test1) ;29.5
+(width-interval add-interval-test);29.5
+
+(define mul-test (mul-interval b a))
+(define div-test (div-interval b a))
+
+(width-interval mul-test) ;475.0
+(width-interval div-test) ;47.5
+
 
 
 
