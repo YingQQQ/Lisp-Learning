7d26e3ef626234ca69a98c97ea138d1bff0803a4
YingQQQ <yingqi2008win@gmail.com>
2018-11-02 16:55:44 +0800

    chapter2-2.39-exercise

--- chapter_2_part4.rkt
+++ chapter_2_part4.rkt
@@ -160,5 +160,109 @@
 
 (accumulate / 1 (list 1 2 3))
 (fold-left / 1 (list 1 2 3))
-(display-info (accumulate list nil (list 1 2 3)))
-(display-info (fold-left list nil (list 1 2 3)))
+(accumulate + 1 (list 1 2 3))
+(fold-left + 1 (list 1 2 3))
+(accumulate * 1 (list 1 2 3))
+(fold-left * 1 (list 1 2 3))
+(accumulate - 1 (list 1 2 3))
+(fold-left - 1 (list 1 2 3))
+;符合结合律的运算都是有相同的结果
+
+
+;exercise-2.39
+
+(define (reverse list)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items)
+              (cons (car items) result))))
+  (iter list nil))
+
+(define (new-reverse-1 seqs)
+  (accumulate (lambda (x y)
+                (append y (list x)))
+              nil
+              seqs))
+(define (new-reverse-2 seqs)
+  (fold-left (lambda (x y)
+              (cons y x))
+              nil
+              seqs))
+                
+(display-info (new-reverse-1 (list 1 2 3 4)))
+                
+(display-info (new-reverse-2 (list 1 2 3 4)))
+
+
+;嵌套映射
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fermat-test n)
+    (define (try-it a)
+        (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+    (cond ((= times 0)
+            true)
+          ((fermat-test n)
+            (fast-prime? n (- times 1)))
+          (else
+            false)))
+
+(define (expmod base exp m)
+    (cond ((= exp 0)
+            1)
+          ((even? exp)
+            (remainder (square (expmod base (/ exp 2) m))
+                       m))
+          (else
+            (remainder (* base (expmod base (- exp 1) m))
+                       m))))
+(define (prime? n)
+    (fast-prime? n 10))
+;抽象append过程
+(define (flatmap proc seq)
+  (accumulate append
+              nil
+              (map proc seq)))
+;最后的过滤条件是 i + j 是否素数,定义谓词：
+(define (prime-sum? pair)
+  (prime? (+ (car pair)
+             (cadr pair))))
+
+;生成三元组
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                   (map (lambda (j)
+                          (list i j))
+                        (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
+
+(display-info (accumulate append
+nil
+(map (lambda (i)
+(map (lambda (j) (list i j))
+(enumerate-interval 1 (- i 1))))
+(list 1 2 3)))) 
+
+(display-info (enumerate-interval 1 3))
+(display-info (enumerate-interval 1 2))
+
+
+
+
+
+
+
+
+
