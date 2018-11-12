08d105d714071c8c656671de6c5e3779394528fb
YingQQQ <yingqi2008win@gmail.com>
2018-11-12 19:33:05 +0800

    chapter2-2.56-exercise

--- chapter_2_part5.rkt
+++ chapter_2_part5.rkt
@@ -328,4 +328,88 @@
                 (paint-down frame)))))
 
 
+;exercise-2.52
+
+;a)
+(segments->painter (list (make-segment (make-vect 0.0 0.0)
+                                       (make-vect 1.0 1.0))
+                         (make-segment (make-vect 0.4 1.0)      ; 头部左上
+                                       (make-vect 0.35 0.85))
+                         (make-segment (make-vect 0.35 0.85)    ; 头部左下
+                                       (make-vect 0.4 0.64))
+                         (make-segment (make-vect 0.4 0.65)     ; 左肩
+                                       (make-vect 0.25 0.65))
+                         (make-segment (make-vect 0.25 0.65)    ; 左手臂上部
+                                       (make-vect 0.15 0.6))
+                         (make-segment (make-vect 0.15 0.6)     ; 左手上部
+                                       (make-vect 0.0 0.85))
+
+                         (make-segment (make-vect 0.0 0.65)     ; 左手下部
+                                       (make-vect 0.15 0.35))
+                         (make-segment (make-vect 0.15 0.35)    ; 左手臂下部
+                                       (make-vect 0.25 0.6))
+
+                         (make-segment (make-vect 0.25 0.6)     ; 左边身体
+                                       (make-vect 0.35 0.5))
+                         (make-segment (make-vect 0.35 0.5)     ; 左腿外侧
+                                       (make-vect 0.25 0.0))
+                         (make-segment (make-vect 0.6 1.0)      ; 头部右上
+                                       (make-vect 0.65 0.85))
+                         (make-segment (make-vect 0.65 0.85)    ; 头部右下
+                                       (make-vect 0.6 0.65))
+                         (make-segment (make-vect 0.6 0.65)     ; 右肩
+                                       (make-vect 0.75 0.65))
+                         (make-segment (make-vect 0.75 0.65)    ; 右手上部
+                                       (make-vect 1.0 0.3))
+
+                         (make-segment (make-vect 1.0 0.15)     ; 右手下部
+                                       (make-vect 0.6 0.5))
+                         (make-segment (make-vect 0.6 0.5)      ; 右腿外侧
+                                       (make-vect 0.75 0.0))
+
+                         (make-segment (make-vect 0.4 0.0)      ; 左腿内侧
+                                       (make-vect 0.5 0.3))
+                         (make-segment (make-vect 0.6 0.0)      ; 右腿内侧
+                                       (make-vect 0.5 0.3))))
+;b)
+(define (corner-split painter n)
+    (if (= n 0)
+        painter
+        (let ((up (up-split painter (- n 1)))
+              (right (right-split painter (- n 1)))
+              (corner (corner-split painter (- n 1))))
+            (beside (below painter up)
+                    (below right corner)))))
+
+;c)
+(define (square-limit painter n)
+    (let ((combine4 (square-of-four identity flip-horiz)
+                                    flip-vect rotate180))
+        (combine4 (corner-split painter n))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                      
---
+++ chapter_2_part6.rkt
@@ -0,0 +1,162 @@
+#lang sicp
+;符号数据
+
+(define a 1)
+(define b 2)
+(define c 3)
+(list a b)
+(list 'a b)
+(list 'a 'b c)
+(car '(a b c))
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+(memq 'apple '(pear banana prune))
+(memq 'apple '(x (apple sauce) y apple pear))
+
+;exercise-2.53
+(list 'a 'b 'c); (a b c)
+(list (list 'george)); ((george))
+(cdr '((x1 x2) (y1 y2))); ((y1 y2))
+(cadr '((x1 x2) (y1 y2))); (y1 y2)
+(pair? (car '(a short list))); #f
+(memq 'red '((red shoes) (blue socks))); #f
+(memq 'red '(red shoes blue socks)); (red shoes blue socks)
+
+;exercise-2.54
+
+(define (equal? item x)
+  (if (and (pair? item) (pair? x)) ;item, x 都是list
+      (if (and (equal? (car item) (car x)))
+           (equal? (cdr item) (cdr x))
+           false)
+      (eq? item x)))
+
+(equal? '(list is a list) '(list is a list))
+
+(equal? '(list is a list) '(list (is a) list))
+
+;exercise-2.55
+(car ''abracdadad)
+;根据97的100注释可以得知'就quote的缩写形式
+
+;2.3.2 实例:符号求导
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var)
+             1
+             0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression tyep -- DERIV" exp))))
+
+(define (variable? exp)
+  (symbol? exp));symbol 判断是不是符号
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (list '+ a1 a2))
+
+(define (make-product m1 m2)
+  (list '* m1 m2))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend a)
+  (cadr a))
+
+(define (augend a)
+  (caddr a))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier a)
+  (cadr a))
+
+(define (multiplicand a)
+  (caddr a))
+
+;对一些特殊运算的简化
+(define (make-sum-better a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-product-better m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0))
+         0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2))
+         (* m1 m2))
+        (else (list '* m1 m2))))
+
+;exercise-2.56
+
+(define (deriv-exercise exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var)
+             1
+             0))
+        ((sum? exp)
+         (make-sum (deriv-exercise (addend exp) var)
+                   (deriv-exercise (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv-exercise (multiplicand exp) var))
+          (make-product (deriv-exercise (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (make-product (exponent exp)
+                        (make-exponentiation (base exp)
+                                             (- (exponent exp) 1)))
+          (deriv (base exp) var)))
+        (else
+         (error "unknown expression tyep -- DERIV" exp))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base x)
+  (cadr x))
+
+(define (exponent p)
+  (caddr p))
+
+(define (make-exponentiation base exponent)
+  (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        (else (list '** base exponent))))
+
+
+
+
+
+
+
+
+
